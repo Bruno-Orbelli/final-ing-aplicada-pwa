067e8602ae8f082bd950da0555b3810ef527012b
Bruno-Orbelli <b.orbelli@alumno.um.edu.ar>
2024-06-27 18:04:44 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'final_ing_aplicada_pwa',
+  webDir: 'dist'
+};
+
+export default config;
